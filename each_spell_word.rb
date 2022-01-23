c4e1f9e9b77cb43dabe2b08f8b7b7eae7ef6c992
trevor-mill <trevor.million@chicagobooth.edu>
2022-01-23 20:08:46 +0000

    word speller finished

--- each_spell_word.rb
+++ each_spell_word.rb
@@ -3,4 +3,9 @@
 # Asks your user to enter any word and have it spelled out letter by letter.  
 
 p "Enter a word for me to spell:"
+word = gets.chomp.upcase.split("")
+
+word.each do |letter|
+  p letter
+end
 
